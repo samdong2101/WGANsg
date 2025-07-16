f280eb462e987ac4ee93a0e95d34764a47945605
samdong2101 <112105146+samdong2101@users.noreply.github.com>
2025-07-16 09:27:29 -0400

    Update generate.py

--- generate.py
+++ generate.py
@@ -1,1 +1,17 @@
+from WGAN_sg import WGAN_sg_model
+from featurize import struct2img
+from post_process import img2struct
+import pickle
+import tensorflow as tf
+import time
+from datetime import date
+from datetime import datetime
+import numpy as np
+import random
+from pymatgen.core.periodic_table import Element
+from scipy.stats import wasserstein_distance
+import argparse
+import json
 
+
+generator = WGAN_sg_model.build_generator()
